d2b2408a32014741101142187f6fb2d6478ade54
wnstjd9701 <wnstjd9701@naver.com>
2023-05-17 19:23:12 +0900

    Update: Main

--- ShoppingMall/src/shoppingmall/model/ShoppingMallDataSource.java
+++ ShoppingMall/src/shoppingmall/model/ShoppingMallDataSource.java
@@ -6,38 +6,38 @@ import java.sql.SQLException;
 import org.apache.commons.dbcp2.BasicDataSource;
 
 public class ShoppingMallDataSource {
-	
+
 	private static final String DRIVER = "oracle.jdbc.OracleDriver";
 	private static final String URL = "jdbc:oracle:thin:@192.168.0.142:1521:xe";
 	private static final String USERNAME = "hr";
 	private static final String PASSWORD = "hr";
-	
+
 	private static BasicDataSource dataSource;
-	
+
 	static {
 		try {
 			dataSource = new BasicDataSource();
-			dataSource.setDriverClassName(DRIVER);            
+			dataSource.setDriverClassName(DRIVER);
 			dataSource.setUrl(URL);
-			dataSource.setUsername(USERNAME);                                  
-			dataSource.setPassword(PASSWORD);  
+			dataSource.setUsername(USERNAME);
+			dataSource.setPassword(PASSWORD);
 			dataSource.setInitialSize(10);
 			dataSource.setMaxTotal(10);
-			System.out.println("good");
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
 	}
-	
+
 	public static Connection getConnection() throws SQLException {
 		return dataSource.getConnection();
 	}
 
 	public static void closeConnection(Connection con) {
-		if(con!=null) {
+		if (con != null) {
 			try {
 				con.close();
-			}catch(Exception e) {}
+			} catch (Exception e) {
+			}
 		}
 	}
 }
